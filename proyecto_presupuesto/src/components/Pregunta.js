2195ba15766b9940de5b4a1c96c309db2b2504b8
rbenitez <rsbenitez@glamit.com.ar>
2020-04-14 21:44:02 -0300

    se agrego la funcion al input definirDpresupuesto e agregar presupuesto queda validar

--- proyecto_presupuesto/src/components/Pregunta.js
+++ proyecto_presupuesto/src/components/Pregunta.js
@@ -1,14 +1,30 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Pregunta = () => {
+  //definimos el state de la cantidad
+
+  const [cantidad, guardadCantidad] = useState(0);
+  // le ponemos 0 por que es una cantidad que se va a ingresar
+
+  //definimos la funcion definirPresupuesto
+
+  const definirPresupuesto = (e) => {
+    guardadCantidad(parseInt(e.target.value, 10));
+  };
+
+  const agregarPresupueto = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <Fragment>
       <h2>Coloca tu presupuesto</h2>
-      <form>
+      <form onSubmit={agregarPresupueto}>
         <input
           type="numbre"
           className="u-full-width"
           placeholder="Coloca tu presupuesto"
+          onChange={definirPresupuesto}
         />
         <input
           type="submit"
