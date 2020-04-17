d680b905441b41a978979daec33aa9e035219fbd
rbenitez <rsbenitez@glamit.com.ar>
2020-04-17 13:54:22 -0300

    nuevos state cantidad y error validando el error

--- proyecto_presupuesto/src/components/Pregunta.js
+++ proyecto_presupuesto/src/components/Pregunta.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import Error from "./Error";
 
 const Pregunta = () => {
   //definimos el state de la cantidad
@@ -6,22 +7,36 @@ const Pregunta = () => {
   const [cantidad, guardadCantidad] = useState(0);
   // le ponemos 0 por que es una cantidad que se va a ingresar
 
-  //definimos la funcion definirPresupuesto
+  const [error, guardarError] = useState(false);
 
+  //definimos la funcion definirPresupuesto
   const definirPresupuesto = (e) => {
     guardadCantidad(parseInt(e.target.value, 10));
   };
 
   const agregarPresupueto = (e) => {
     e.preventDefault();
+
+    //validar
+    // hacemos un if por que el valor inicial de cantidad es 0
+
+    if (cantidad < 1 || isNaN(cantidad)) {
+      guardarError(true);
+      return;
+    }
+    // si se pasa la validacion
+    guardarError(false);
   };
 
   return (
     <Fragment>
       <h2>Coloca tu presupuesto</h2>
+
+      {error === true ? <Error mensaje="El presupuesto es Incorrecto" /> : null}
+
       <form onSubmit={agregarPresupueto}>
         <input
-          type="numbre"
+          type="number"
           className="u-full-width"
           placeholder="Coloca tu presupuesto"
           onChange={definirPresupuesto}
